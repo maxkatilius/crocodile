7d26c82fb04320ac3fab27bbde8679cfc9d6ca73
maxkatilius <max.katilius@gmail.com>
2025-07-27 17:51:34 +1000

    cors added in server and updated console.log

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const cors = require('cors')
 const dotenv = require('dotenv')
-
 dotenv.config()
 
+const express = require('express')
 const app = express()
-app.use(cors())
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors({
+  origin: process.env.PG_BASE_API_URL,
+}))
 
 // Routes
 app.use("/api/products", require("./routes/products"))
@@ -16,5 +18,5 @@ app.use("/api/order", require("./routes/order"))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`)
+  console.log(`✅ Server running on port ${PORT}`)
 })
